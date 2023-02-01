0d749eb7706087203e78bffaa46bb936f7e4f115
lev-enikeev <l.yenikeev@gmail.com>
2023-02-01 13:56:40 +0300

    added redis

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+# redis://default:redispw@localhost:32768
+import redis
 import discord
 from bot_token import API_TOKEN
 from discord.ext import commands
@@ -6,32 +8,41 @@ intents = discord.Intents.default()
 intents.message_content = True
 bot = commands.Bot(command_prefix='>', intents=intents)
 
-accounts = [' ', 'Minecraft', 'brawl stars']
-state = None
+r = redis.Redis(host='localhost', 
+                port=32768, 
+                db=0, 
+                username='default', 
+                password='redispw',
+                decode_responses=True,
+                charset="utf-8")
 
-@bot.event
-async def on_message(message):
-
-    if message.author == bot.user:
-        return
-    if state == 'sell':
-        for account in accounts:
-            if message.content == account:
-                await message.channel.send('Введите описание')   
-                return
-        await message.channel.send('Такой игры я не знаю')   
-
-    await message.channel.send('Через меня можно продавать и покупать аккаунты в играх')   
+accounts = ['Genshin Impact', 'Minecraft', 'brawl stars']
 
 
 @bot.command(name='sell')
 async def test(ctx):
-    global state
-    state = 'sell'
+    r.set('state', 'sell-description')
     await ctx.send('В какой игре вы хотите продать аккаунт?')
     
 
+@bot.event
+async def on_message(message):
+    if message.author == bot.user:
+        return
+    if r.get('state') == 'sell-description':
+        for account in accounts:
+            if message.content == account:
+                await message.channel.send('Введите описание')   
+                return
+            r.set('state', 'sell-price')
+        await message.channel.send('Такой игры я не знаю')   
+        return
+    if r.get('state') == 'sell-price':
+        await message.channel.send('Введите цену')   
+        return
 
+    await message.channel.send('Через меня можно продавать и покупать аккаунты в играх')   
+    await bot.process_commands(message)
 
 
 bot.run(API_TOKEN)
